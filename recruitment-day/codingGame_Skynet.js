9fdd08e67a840e58aecb404b187968efe75ec903
AbuGitLab <abu.softeng@gmail.com>
2020-03-26 15:52:38 +0530

    Updating the third test-case and es6 formats

--- recruitment-day/codingGame_Skynet.js
+++ recruitment-day/codingGame_Skynet.js
@@ -2,7 +2,7 @@
   Coding Name: Skynet Revolution - Episode 1
   Complexlevel: Medium
   Game Link: https://www.codingame.com/ide/puzzle/skynet-revolution-episode-1
-  Test Case Success: 'working'(2/4)
+  Test Case Success: 3/4
 */
 
 /**Note: 'console.error' is used for Debugging purpose and never mind**/
@@ -15,10 +15,11 @@ var inputs = readline().split(' ');
 var N = parseInt(inputs[0]); // the total number of nodes in the level, including the gateways
 var L = parseInt(inputs[1]); // the number of links
 var E = parseInt(inputs[2]); // the number of exit gateways
+var nodes = {};
+var gateways = [];
 console.error('inputs: ', inputs)
 
 // List the network
-var nodes = {};
 for (let index = 0; index < L; index++) {
   inputs = readline().split(' ');
   let N1 = parseInt(inputs[0]); // N1 and N2 defines a link between these nodes
@@ -41,24 +42,23 @@ for (let index = 0; index < L; index++) {
 }
 
 // List all the exit gateways
-var gateways = [];
 for (let index = 0; index < E; index++) {
   let EI = parseInt(readline()); // the index of a gateway node
   console.error('EI: ', EI)
   gateways.push(EI);
 }
 
-/**
+/*
   Run through all paths to a gateway
   without running over several times on same nodes
- **/
-function searchGatewayInSkynet(SI, path = []) {
+ */
+searchGatewayInSkynet = (SI, path = []) => {
   let result = [];
-  let skyNetLen = nodes[SI].length;
+  let skyNet = nodes[SI];
   path.push(SI);
 
-  for (let index = 0; index < skyNetLen; index++) {
-    let node = nodes[SI][index];
+  for (let index in skyNet) {
+    let node = skyNet[index];
     let predictPath = JSON.parse(JSON.stringify(path)); // JSON.stringify then JSON.parse to clone the object
     if (gateways.indexOf(node) === -1 && path.indexOf(node) === -1) {
       let tmpResult = searchGatewayInSkynet(node, predictPath);
@@ -74,17 +74,17 @@ function searchGatewayInSkynet(SI, path = []) {
 }
 
 //Get the fastest path to a gateway from a list of path
-function getClosetGatewayPath(SI) {
+getClosetGatewayPath = (SI) => {
   let result = searchGatewayInSkynet(SI);
+  result.sort((nodeA, nodeB) => nodeA.length > nodeB.length ? 1 : -1);
   console.error("result", result)
   return result[0];
 }
 
-
 // game loop
 // Display the result in the standard output
 while (true) {
-  var SI = parseInt(readline()); // The index of the node on which the Skynet agent is positioned this turn
-  var result = getClosetGatewayPath(SI);
+  let SI = parseInt(readline()); // The index of the node on which the Skynet agent is positioned this turn
+  let result = getClosetGatewayPath(SI);
   console.log(result[0] + ' ' + result[1]);
 }
